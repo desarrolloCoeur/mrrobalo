faeb6c54382d90313c0b358bb534999946bb53d8
dohed <desarrollo@coeurmkt.com>
2025-09-29 13:45:49 -0700

    added images

--- app/page.js
+++ app/page.js
@@ -2,28 +2,23 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      {/* Mobile image */}
-      <div className="block md:hidden">
+    <div className="bg-gray-400 flex flex-col items-center gap-6 p-4">
+      <div className="w-full max-w-[600px]">
         <Image
-          src="/mobile-menu.jpg"
-          alt="Mobile version"
-          width={300}
-          height={300}
-          className="rounded-lg"
-          priority
+          src="/assets/menu web-01.png"
+          alt="Menu page 1"
+          width={800}
+          height={2000}
+          className="rounded-lg w-full h-auto"
         />
       </div>
-
-      {/* Desktop image */}
-      <div className="hidden md:block">
+      <div className="w-full max-w-[600px]">
         <Image
-          src="/desktop-menu.jpg"
-          alt="Desktop version"
-          width={600}
-          height={400}
-          className="rounded-lg"
-          priority
+          src="/assets/menu web-02.png"
+          alt="Menu page 2"
+          width={800}
+          height={2000}
+          className="rounded-lg w-full h-auto"
         />
       </div>
     </div>
